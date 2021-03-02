d4f5ab87e0961385ac51a4a8b24349b40392229e
Karen Li <karenjy.li@gmail.com>
2021-03-01 22:27:24 -0600

    Add button component

---
+++ disd-map-app/src/components/buttons.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const DistrictButtons = () => {
+    return(
+        <div className="district">
+            <button>District 1</button>
+        </div>
+    )
+}
+
+export default DistrictButtons;
