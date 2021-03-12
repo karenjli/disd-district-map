4adf9a8fc88c7cdb7143e4d8ad4cb99d791503a5
Karen Li <karenjy.li@gmail.com>
2021-03-11 22:40:18 -0600

    Add district buttons

--- disd-map-app/src/components/buttons.js
+++ disd-map-app/src/components/buttons.js
@@ -1,9 +1,19 @@
-import React from "react"
+import React from "react";
+import {Button} from 'reactstrap';
 
 const DistrictButtons = () => {
     return(
         <div className="district">
-            <button>District 1</button>
+            <Button outline color="primary">District 1</Button>
+            <Button>District 2</Button>
+            <Button outline color="primary">District 3</Button>
+            <Button>District 4</Button>
+            <Button>District 5</Button>
+            <Button>District 6</Button>
+            <Button>District 7</Button>
+            <Button>District 8</Button>
+            <Button outline color="primary">District 9</Button>
+
         </div>
     )
 }
